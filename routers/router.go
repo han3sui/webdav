d370455f05774eebfc006ef160cbc73cae0a6949
hanxinxin <peter.han@71360.com>
2021-07-10 10:50:01 +0800

    init

--- routers/router.go
+++ routers/router.go
@@ -28,8 +28,19 @@ func InitRouter() http.Handler {
 	v1 := r.Group(lib.Config.Server.Route)
 	v1.Use(middleware.Auth())
 	{
+		//v1.Handle("PROPFIND", "/*path", pkg.InitWebdav)
+		//v1.Handle("PROPFIND", "", pkg.InitWebdav)
+
+		v1.Any("/*path", pkg.InitWebdav)
+		v1.Any("", pkg.InitWebdav)
 		v1.Handle("PROPFIND", "/*path", pkg.InitWebdav)
-		v1.Handle("PROPFIND","",pkg.InitWebdav)
+		v1.Handle("PROPFIND", "", pkg.InitWebdav)
+		v1.Handle("MKCOL", "/*path", pkg.InitWebdav)
+		v1.Handle("LOCK", "/*path", pkg.InitWebdav)
+		v1.Handle("UNLOCK", "/*path", pkg.InitWebdav)
+		v1.Handle("PROPPATCH", "/*path", pkg.InitWebdav)
+		v1.Handle("COPY", "/*path", pkg.InitWebdav)
+		v1.Handle("MOVE", "/*path", pkg.InitWebdav)
 	}
 	return r
 }
